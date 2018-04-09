12fb7b4654df0625111e5d915b07cf332e64a081
sizmailov <sergei.a.izmailov@gmail.com>
2018-04-10 02:07:18 +0300

    add ShortAsciiString

---
+++ include/xmol/utils/ShortAsciiString.h
@@ -0,0 +1,138 @@
+#pragma once
+
+#include <string>
+#include <cstdint>
+#include <stdexcept>
+#include <type_traits>
+
+#include <cassert>
+
+namespace xmol{
+namespace utils{
+
+template<int MAX_LENGTH, typename=void>
+class ShortAsciiString {
+
+public:
+
+  static_assert(MAX_LENGTH<=8,"");
+  using uint_type = typename std::conditional< MAX_LENGTH<=4,
+                                               typename std::conditional<MAX_LENGTH<=2,
+                                                                         typename std::conditional<MAX_LENGTH<=1,
+                                                                                                   uint8_t,
+                                                                                                   uint16_t>::type,
+                                                                         uint32_t>::type,
+                                               uint64_t>::type;
+  static const int max_length = MAX_LENGTH;
+
+  constexpr ShortAsciiString() : m_value(0) {};
+
+  explicit ShortAsciiString(const std::string& aName) : m_value(to_uint(aName.c_str())){
+  }
+
+  constexpr explicit ShortAsciiString(const char* aName) : m_value(to_uint(aName)) {
+  }
+
+  constexpr explicit ShortAsciiString(const char* aName, int exact_length) : m_value(to_uint(aName,exact_length)) {
+  }
+
+  constexpr ShortAsciiString(const ShortAsciiString& other) : m_value(other.m_value){
+  };
+  constexpr ShortAsciiString(ShortAsciiString&& other) : m_value(other.m_value){
+    other.m_value=0;
+  };
+
+
+  constexpr ShortAsciiString& operator=(const ShortAsciiString& other){ m_value = other.m_value; return *this;}
+  constexpr ShortAsciiString& operator=(ShortAsciiString&& other){ m_value = other.m_value; other.m_value = 0; return *this;}
+
+  ShortAsciiString& operator=(const std::string& aName){
+    m_value = to_uint(aName.c_str());
+    return *this;
+  }
+  constexpr ShortAsciiString& operator=(const char* aName){
+    m_value = to_uint(aName);
+    return *this;
+  }
+
+  constexpr bool operator==(const ShortAsciiString& other) const {
+    return m_value == other.m_value;
+  }
+  constexpr bool operator!=(const ShortAsciiString& other) const {
+    return m_value != other.m_value;
+  }
+  constexpr bool operator==(const char* other) const {
+    return m_value == ShortAsciiString(other).m_value;
+  }
+  constexpr bool operator!=(const char* other) const {
+    return m_value != ShortAsciiString(other).m_value;
+  }
+  constexpr bool operator<(const ShortAsciiString& other) const {
+    return m_value < other.m_value;
+  }
+  constexpr bool operator>(const ShortAsciiString& other) const {
+    return m_value > other.m_value;
+  }
+
+  std::string str() const {
+
+    uint_type tmp = m_value;
+    uint32_t length = 0;
+    while(tmp>0){tmp/=256;length+=1;}
+
+    std::string result;
+    result.resize(length);
+
+    tmp = m_value;
+    for (int i=length-1;i>-1;i--){
+      result[i] = char(tmp%256);
+      tmp /= 256;
+    }
+    return result;
+  }
+
+  explicit operator std::string() const {
+    return this->str();
+  }
+  constexpr uint_type value() const {
+    return m_value;
+  }
+
+private:
+  constexpr uint_type static to_uint(const char* aName){
+    uint_type m_value = 0;
+    const char* origin=aName;
+    for (int i = 0; *aName != '\0' && i < MAX_LENGTH; ++i, ++aName) {
+      m_value = m_value * 256 + uint_type(*aName);
+    }
+    return (*aName == '\0')
+           ? m_value
+           : throw std::runtime_error("ShortName::too_long: len(`"+std::string(origin,MAX_LENGTH)+"`...) > "+std::to_string(MAX_LENGTH));
+  }
+  constexpr uint_type static to_uint(const char* aName, int exact_length){
+    assert(exact_length <=MAX_LENGTH);
+    uint_type m_value = 0;
+    const char* end=aName+exact_length;
+    for (; aName!=end; ++aName) {
+      m_value = m_value * 256 + uint_type(*aName);
+    }
+    return m_value;
+  }
+  uint_type m_value;
+};
+
+}
+}
+
+namespace std {
+template <int N,typename T>
+struct hash<xmol::utils::ShortAsciiString<N,T>> {
+  using argument_type = xmol::utils::ShortAsciiString<N,T>;
+  using result_type = size_t;
+
+  result_type operator()(argument_type const& s) const {
+    return s.value();
+  }
+};
+}
+
