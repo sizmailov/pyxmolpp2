a8f50caff6493c05b92a3f29f55fe775551b9930
sizmailov <sergei.a.izmailov@gmail.com>
2018-04-13 09:39:07 +0300

    add build commands to coverage script

--- run_gcovr.sh
+++ run_gcovr.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+make -C cmake-build-coverage all 
+make -C cmake-build-coverage test 
+
 gcovr --root=./ --exclude='external/.*' --exclude='tests/.*' --html --html-details  -o coverage/xmol.html
