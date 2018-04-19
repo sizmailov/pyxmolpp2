c543df6b99c28a49cae6e37a6068a2d06da9d75c
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2018-04-19 13:41:19 +0300

    fix run_gcovr.sh: create ./coverage, use right one pytest

--- run_gcovr.sh
+++ run_gcovr.sh
@@ -4,6 +4,7 @@ make -C cmake-build-coverage all
 make -C cmake-build-coverage test 
 
 export PYTHONPATH=cmake-build-coverage
-pytest pytests
+python -m pytest pytests
 
+mkdir -p coverage 
 gcovr --root=./ --exclude='external/.*' --exclude='tests/.*' --html --html-details  -o coverage/xmol.html
