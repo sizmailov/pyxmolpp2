bc4b9b0acd78bc8b4bf416e9f353888035fdc1ee
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2018-04-19 17:13:20 +0300

    bind geometry::basic, add tests

--- pyxmolpp/geometry/basic.cpp
+++ pyxmolpp/geometry/basic.cpp
@@ -1,7 +1,12 @@
 #include "init.h"
 
-#include "xmol/geometry/XYZ.h"
+#include "xmol/geometry/basic.h"
+
+using namespace xmol::geometry;
 
 void pyxmolpp::geometry::init_basic(pybind11::module& geometry) {
-
+  geometry.def("distance",&xmol::geometry::distance);
+  geometry.def("distance2",&xmol::geometry::distance2);
+  geometry.def("angle",&xmol::geometry::angle);
+  geometry.def("dihedral_angle",&xmol::geometry::dihedral_angle);
 }
