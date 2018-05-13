d3fb6d80cffd3307ba22eea093003e1460445814
sizmailov <sergei.a.izmailov@gmail.com>
2018-05-13 20:13:01 +0300

    make tag version main

--- build_docs.sh
+++ build_docs.sh
@@ -2,11 +2,12 @@
 
 function doCompile {
   pip install -r docs/requirements.txt
-  sphinx-versioning build -r master \
+  sphinx-versioning build \
       docs/source docs/build/html \
       --run-setup-py \
       --whitelist-branches 'master|dev' \
       --whitelist-tags '.*' \
       --show-banner \
-      --greatest-tag
+      --greatest-tag \
+      --banner-greatest-tag
 }
