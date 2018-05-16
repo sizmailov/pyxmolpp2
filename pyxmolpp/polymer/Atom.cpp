382dc4f8bd2a315865cf7a3ac72107edb8455ea5
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2018-05-16 19:54:34 +0300

    clean up pyxmolpp bindings

--- pyxmolpp/polymer/Atom.cpp
+++ pyxmolpp/polymer/Atom.cpp
@@ -7,22 +7,6 @@
 #include "xmol/polymer/Atom.h"
 #include "xmol/utils/string.h"
 
-#define POLYMER_USE_SMART_POINTERS
-//
-// PYBIND11_MAKE_OPAQUE(xmol::polymer::Frame);
-// PYBIND11_MAKE_OPAQUE(xmol::polymer::ElementReference<xmol::polymer::Atom>);
-// PYBIND11_MAKE_OPAQUE(xmol::polymer::ElementReference<xmol::polymer::Residue>);
-// PYBIND11_MAKE_OPAQUE(xmol::polymer::ElementReference<xmol::polymer::Chain>);
-
-#ifndef POLYMER_USE_SMART_POINTERS
-PYBIND11_MAKE_OPAQUE(xmol::polymer::Atom);
-PYBIND11_MAKE_OPAQUE(xmol::polymer::Residue);
-PYBIND11_MAKE_OPAQUE(xmol::polymer::Chain);
-PYBIND11_MAKE_OPAQUE(xmol::polymer::AtomSelection);
-PYBIND11_MAKE_OPAQUE(xmol::polymer::ResidueSelection);
-PYBIND11_MAKE_OPAQUE(xmol::polymer::ChainSelection);
-#endif
-
 void pyxmolpp::polymer::init_Atom(pybind11::module& polymer) {
 
   using namespace xmol::polymer;
@@ -32,42 +16,17 @@ void pyxmolpp::polymer::init_Atom(pybind11::module& polymer) {
   auto&& pyResidueName = py::class_<ResidueName>(polymer, "ResidueName");
   auto&& pyChainName = py::class_<ChainName>(polymer, "ChainName");
 
-#ifdef POLYMER_USE_SMART_POINTERS
   using AtomType = ElementReference<Atom>;
   using ResidueType = ElementReference<Residue>;
   using ChainType = ElementReference<Chain>;
   using FrameType = Frame;
-#else
-  using AtomType = Atom;
-  using ResidueType = Residue;
-  using ChainType = Chain;
-  using FrameType = Frame;
-#endif
 
-  using AtomRef = std::conditional<std::is_same<AtomType, Atom>::value, Atom&, ElementReference<Atom>>::type;
-  using ResidueRef =
-      std::conditional<std::is_same<ResidueType, Residue>::value, Residue&, ElementReference<Residue>>::type;
-  using ChainRef = std::conditional<std::is_same<ChainType, Chain>::value, Chain&, ElementReference<Chain>>::type;
   using FrameRef = Frame&;
 
-  constexpr py::return_value_policy AtomRefPolicy =
-      std::is_same<AtomType, Atom>::value ? py::return_value_policy::reference : py::return_value_policy::move;
-  constexpr py::return_value_policy ResidueRefPolicy =
-      std::is_same<ResidueType, Residue>::value ? py::return_value_policy::reference : py::return_value_policy::move;
-  constexpr py::return_value_policy ChainRefPolicy =
-      std::is_same<ChainType, Chain>::value ? py::return_value_policy::reference : py::return_value_policy::move;
-  constexpr py::return_value_policy FrameRefPolicy =
-      std::is_same<FrameType, Frame>::value ? py::return_value_policy::reference : py::return_value_policy::move;
-
-  static_assert(!std::is_same<AtomType, Atom>::value, "");
-  static_assert(!std::is_same<ResidueType, Residue>::value, "");
-  static_assert(!std::is_same<ChainType, Chain>::value, "");
-  static_assert(std::is_same<FrameType, Frame>::value, "");
-
-  static_assert(std::is_same<AtomRef, ElementReference<Atom>>::value, "");
-  static_assert(std::is_same<ResidueRef, ElementReference<Residue>>::value, "");
-  static_assert(std::is_same<ChainRef, ElementReference<Chain>>::value, "");
-  static_assert(std::is_same<FrameRef, Frame&>::value, "");
+  constexpr py::return_value_policy AtomRefPolicy = py::return_value_policy::reference;
+  constexpr py::return_value_policy ResidueRefPolicy = py::return_value_policy::reference;
+  constexpr py::return_value_policy ChainRefPolicy = py::return_value_policy::reference;
+  constexpr py::return_value_policy FrameRefPolicy = py::return_value_policy::reference;
 
   auto&& pyAtom = py::class_<AtomType>(polymer, "Atom");
   auto&& pyResidue = py::class_<ResidueType>(polymer, "Residue");
