d753a1f1745a1237fbcd6c79814d624ca87a0562
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-02-19 13:02:46 +0300

    fix(bindings): fix AtomSelection.align_to, .aligment_to

--- pytests/xmol/polymer/test_shortcuts.py
+++ pytests/xmol/polymer/test_shortcuts.py
@@ -30,9 +30,11 @@ def make_polyglycine(chain_lengths, no_reserve=True):
 def test_shorthands():
     import numpy as np
 
-    from pyxmolpp2.geometry import Rotation3d, Translation3d, XYZ, Degrees
+    from pyxmolpp2.geometry import Rotation3d, Translation3d, XYZ, Degrees, calc_alignment
 
     frame = make_polyglycine([("A", 20)])
+    for a in frame.asAtoms:
+        a.r = XYZ(*np.random.random(3))
     frame2 = frame.copy()
 
     asel = frame.asAtoms
@@ -46,9 +48,6 @@ def test_shorthands():
     T = Translation3d(XYZ(1, 0, 0))
     asel2.transform(T)
 
-    # print(list(asel.toCoords))
-    # print(list(asel2.toCoords))
-
     assert np.isclose(asel.rmsd(asel2), 1.0)
     assert np.isclose(asel.rmsd(asel2.toCoords), 1.0)
     assert np.isclose(asel.rmsd(asel2.toCoords.transform(T.inverted())), 0.0)
@@ -60,3 +59,10 @@ def test_shorthands():
     asel2.transform(T)
     asel.align_to(asel2.toCoords)
     assert np.isclose(asel.rmsd(asel2), 0)
+
+    T = Translation3d(XYZ(1, 0, 0)) * Rotation3d(XYZ(1, 1, 1), Degrees(45))
+    asel2.transform(T)
+
+    assert np.allclose(T.matrix3d(), calc_alignment(asel2.toCoords, asel.toCoords).matrix3d())
+    assert np.allclose(T.matrix3d(), asel.alignment_to(asel2).matrix3d())
+    assert np.allclose(T.matrix3d(), asel.alignment_to(asel2.toCoords).matrix3d())
