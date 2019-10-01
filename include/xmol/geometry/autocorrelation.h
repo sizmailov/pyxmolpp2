09814b1c8dc05693a80466bf86ce5d117f5cb3c2
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-10-01 18:13:23 +0300

    Add auto correlation function calculator for PRE

--- include/xmol/geometry/autocorrelation.h
+++ include/xmol/geometry/autocorrelation.h
@@ -4,10 +4,19 @@
 #include <complex>
 #include <vector>
 
-namespace xmol{
-namespace geometry{
+namespace xmol {
+namespace geometry {
 
-std::vector<double> calc_autocorr_order_2(const std::vector<XYZ>& v, long long limit=-1);
+enum class AutoCorrelationMode {
+  NORMALIZE_VECTORS,
+  NORMALIZE_AND_DIVIDE_BY_CUBE
+};
+
+std::vector<double> calc_autocorr_order_2(
+    const std::vector<XYZ> &v,
+    long long limit = -1,
+    const AutoCorrelationMode &mode = AutoCorrelationMode::NORMALIZE_VECTORS
+);
 
 }
 }
