d8afc0c31ea68c07836787a60217ffbeddebd345
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-05-29 08:18:26 +0300

    Fix doc gen

--- docs/build.sh
+++ docs/build.sh
@@ -6,9 +6,9 @@ wd="$PWD"
 export TEST_DATA_PATH="$PWD/../tests_dataset"
 
 if [[ ! -d m.css ]]; then
-  # TODO: switch to official repo once PR#143 is accepted
+  # TODO: switch to official repo once PR#143, PR#145 accepted
   # git clone git://github.com/mosra/m.css
-  git clone --depth 1  --branch default-argument-parsing --single-branch --origin sizmailov git://github.com/sizmailov/m.css
+  git clone --depth 1  --branch all-fixes --single-branch --origin sizmailov git://github.com/sizmailov/m.css
 fi
 
 cd m.css/documentation
