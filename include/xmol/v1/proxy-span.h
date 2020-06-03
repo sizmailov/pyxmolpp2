dd1274eb0052a0aaad65f871d3645367845c925b
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-06-03 08:07:39 +0300

    Inline ProxySpan methods

--- include/xmol/v1/proxy-span.h
+++ include/xmol/v1/proxy-span.h
@@ -8,32 +8,38 @@ template <typename Proxy, typename T> class ProxySpan {
 public:
   class Iterator {
   public:
-    Iterator(T* ptr, T* end);
-    Proxy* operator->();
-    Proxy& operator*();
-    Iterator& operator++();
-    bool operator!=(const Iterator& other);
+    Iterator(T* ptr, T* end) : p(ptr, end){};
+    Proxy* operator->() { return &p; };
+    Proxy& operator*() { return p; };
+    void operator++() { p.advance(); } // doesn't return iterator
+    bool operator!=(const Iterator& other) { return p != other.p; }
 
   private:
     Proxy p;
   };
   ProxySpan() = default;
-  explicit ProxySpan(const future::Span<T>& span);
-  ProxySpan(T* b, T* e);
-  ProxySpan(T* b, size_t n);
+  explicit ProxySpan(const future::Span<T>& span) : m_begin(span.m_begin), m_end(span.m_end){};
+  ProxySpan(T* b, T* e) : m_begin(b), m_end(e){};
+  ProxySpan(T* b, size_t n) : m_begin(b), m_end(b + n){};
 
-  [[nodiscard]] size_t size() const;
-  [[nodiscard]] size_t empty() const;
+  [[nodiscard]] size_t size() const { return m_end - m_begin; };
+  [[nodiscard]] size_t empty() const { return m_end == m_begin; };
 
-  [[nodiscard]] Proxy operator[](size_t i);
+  [[nodiscard]] Proxy operator[](size_t i) {
+    assert(i < size());
+    return Proxy(*(m_begin + i));
+  }
 
-  [[nodiscard]] Iterator begin();
-  [[nodiscard]] Iterator end();
+  [[nodiscard]] Iterator begin() { return Iterator(m_begin, m_end); }
+  [[nodiscard]] Iterator end() { return Iterator(m_end, m_end); }
 
 protected:
-  void rebase(T* from, T* to);
+//  inline void rebase(T* from, T* to) {
+//    m_begin = to + (m_begin - from);
+//    m_end = to + (m_end - from);
+//  }
 
-private:
+public:
   T* m_begin = nullptr;
   T* m_end = nullptr;
 };
