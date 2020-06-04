b5f122c1157df59e1636d451b2cd878442f28c62
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-06-04 15:18:15 +0300

    Document base.h

--- include/xmol/v1/base.h
+++ include/xmol/v1/base.h
@@ -2,31 +2,28 @@
 #include "future/span.h"
 #include "fwd.h"
 
-namespace xmol {
-namespace v1 {
+namespace xmol::v1 {
 
-struct BaseAtom;
-struct BaseResidue;
-struct BaseMolecule;
-
+/// Storage of atomic data except coords
 struct BaseAtom {
-  AtomName name;
-  AtomId id;
-  BaseResidue* residue = nullptr;
+  AtomName name;                  /// Atom name
+  AtomId id;                      /// Atom id
+  BaseResidue* residue = nullptr; /// Parent residue
 };
 
+/// Storage of residue data
 struct BaseResidue {
-  ResidueName name;
-  ResidueId id;
-  future::Span<BaseAtom> atoms;
-  BaseMolecule* molecule = nullptr;
+  ResidueName name;                 /// Residue name
+  ResidueId id;                     /// Residue id
+  future::Span<BaseAtom> atoms;     /// Children atoms
+  BaseMolecule* molecule = nullptr; /// Parent molecule
 };
 
+/// Storage of molecule data
 struct BaseMolecule {
-  Frame* frame;
-  MoleculeName name;
-  future::Span<BaseResidue> residues;
+  Frame* frame;                       /// Parent frame
+  MoleculeName name;                  /// Molecule name
+  future::Span<BaseResidue> residues; /// Children residues
 };
 
-} // namespace v1
-} // namespace xmol
+} // namespace xmol::v1
