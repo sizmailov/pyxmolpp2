5f07dc69b33917e641f0e948e2b4b742b2f4291f
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-06-06 16:05:04 +0300

    Add basic trajectory primitives

---
+++ include/xmol/v1/traj/Trajectory.h
@@ -0,0 +1,99 @@
+#pragma once
+#include "../Frame.h"
+#include "TrajectoryFile.h"
+
+namespace xmol::v1::traj {
+
+using FrameId = int32_t;
+
+class Trajectory {
+  struct Position {
+    size_t global_pos;  // global frame index in trajectory
+    size_t file;        // number of input file
+    size_t pos_in_file; // position in input file
+  };
+
+public:
+  class Frame : public v1::Frame {
+  public:
+    Frame() = default;
+    Frame(Frame&&) = default;
+    Frame(const Frame&) = default;
+    explicit Frame(v1::Frame frame) : v1::Frame(std::move(frame)), id(0){};
+    FrameId id = 0;
+  };
+
+  struct Sentinel {};
+
+  class Iterator {
+  public:
+    Frame& operator*() { return m_frame; }
+    Frame* operator->() { return &m_frame; }
+    Iterator& operator++() {
+      update();
+      m_traj.advance(m_pos, m_step);
+    }
+
+    bool operator!=(const Sentinel&) const { return m_pos.global_pos < m_end; }
+
+  private:
+    Iterator(Trajectory& t, Position begin, size_t end, size_t step)
+        : m_traj(t), m_pos(begin), m_end(end), m_step(step), m_frame(t.m_frame) {
+      assert(step > 0);
+      if (m_pos.global_pos < m_end) {
+        update();
+      }
+    }
+    void update() {
+      auto coords = m_frame.coordinates();
+      m_traj.read_coordinates(m_pos, coords);
+      m_frame.id = m_pos.global_pos;
+    }
+    Trajectory& m_traj;
+    Position m_pos;
+    size_t m_end;
+    size_t m_step;
+    Frame m_frame;
+  };
+
+  Trajectory() = delete;
+  explicit Trajectory(Frame&& frame) : m_frame(std::move(frame)){};
+  Trajectory(Trajectory&& other) = default;
+  Trajectory& operator=(Trajectory&& other) = default;
+
+  Trajectory(const Trajectory& other) = delete;
+  Trajectory& operator=(const Trajectory& other) = delete;
+
+  void extend(std::unique_ptr<TrajectoryInputFile>&& input_file) {
+    assert(n_atoms() == input_file->n_atoms());
+    m_n_frames += input_file->n_frames();
+    m_files.push_back(std::move(input_file));
+  }
+
+  [[nodiscard]] size_t n_frames() const { return m_n_frames; };
+  [[nodiscard]] size_t n_atoms() const { m_frame.n_atoms(); }
+
+protected:
+  v1::Frame m_frame;
+  size_t m_n_frames = 0;
+  std::vector<std::unique_ptr<TrajectoryInputFile>> m_files;
+  void read_coordinates(Position pos, future::Span<XYZ>& coords) {
+    m_files[pos.file]->read_coordinates(pos.pos_in_file, coords);
+  }
+
+  void advance(Position& position, size_t step) {
+    position.global_pos + step;
+    if (position.global_pos > n_frames()) {
+      return;
+    }
+    position.pos_in_file += step;
+    m_files[position.file]->advance(step);
+    while (position.file < m_files.size() && position.pos_in_file >= m_files[position.file]->n_frames()) {
+      position.pos_in_file -= m_files[position.file]->n_frames();
+      position.file++;
+    }
+    assert(position.file < m_files.size() && position.pos_in_file < m_files[position.file]->n_frames());
+  }
+};
+
+} // namespace xmol::v1::traj
---
+++ include/xmol/v1/traj/TrajectoryFile.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "xmol/v1/future/span.h"
+#include "xmol/v1/fwd.h"
+
+namespace xmol::v1::traj {
+
+class TrajectoryInputFile {
+public:
+  [[nodiscard]] virtual size_t n_frames() const = 0;
+  [[nodiscard]] virtual size_t n_atoms() const = 0;
+  virtual size_t current_frame_index() const = 0;
+  virtual void read_coordinates(size_t index, future::Span<XYZ>& coordinates) = 0;
+
+  void advance(size_t n) {
+    assert(n == current_frame_index());
+    advance_impl(n);
+  }
+
+protected:
+  virtual void advance_impl(int n) = 0;
+};
+
+} // namespace xmol::v1::traj
