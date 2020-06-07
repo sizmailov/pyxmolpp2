1b331fe5e5a05a4c31214594283e9dfb1c0751f5
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-06-07 14:51:20 +0300

    Read real frame in v1 trajectory benchmark

--- tests/benchmarks/v1/iterate-trajectory.h
+++ tests/benchmarks/v1/iterate-trajectory.h
@@ -3,6 +3,7 @@
 #include "common.h"
 #include "xmol/v1/trajectory/Trajectory.h"
 #include "xmol/v1/trajectory/TrjtoolDatFile.h"
+#include "xmol/v1/io/PdbFile.h"
 
 #include <cassert>
 #include <cstdlib>
@@ -12,17 +13,14 @@ using namespace trajectory;
 class BM_TrajectoryTrjtool : public benchmark::Fixture {
 public:
   void SetUp(const ::benchmark::State& state) {
+    using xmol::v1::io::PdbFile;
     char* data_path = std::getenv("TEST_DATA_PATH");
     assert(data_path);
     const std::string filename = std::string(data_path) + "/trjtool/GB1/run00001.dat";
+    const std::string pdb_filename = std::string(data_path) + "/trjtool/GB1/run00001.pdb";
 
-    Frame frame;
-    auto mol = frame.add_molecule({});
-    auto res = mol.add_residue({}, {});
-    const int n = 880;
-    for (int i = 0; i < n; ++i) {
-      res.add_atom({}, {});
-    }
+    Frame frame = PdbFile(pdb_filename).read(PdbFile::Dialect::AMBER_99).frames()[0];
+
     trj_ptr = std::make_unique<Trajectory>(std::move(frame));
     trj_ptr->extend(TrjtoolDatFile(filename));
     trj_ptr->extend(TrjtoolDatFile(filename));
