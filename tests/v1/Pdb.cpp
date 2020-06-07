efd8812963e604ada98bed2f6ac26229590a62f2
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-06-07 14:49:29 +0300

    Test pdb read

---
+++ tests/v1/Pdb.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+#include "xmol/v1/Frame.h"
+#include "xmol/v1/io/PdbFile.h"
+
+using ::testing::Test;
+using namespace xmol::v1;
+using namespace xmol::v1::proxy;
+using namespace xmol::v1::proxy::smart;
+using namespace xmol::v1::io;
+
+class PdbTests : public Test {};
+
+TEST_F(PdbTests, read) {
+  const std::string filename("trjtool/GB1/run00001.pdb");
+  Frame frame = PdbFile(filename).read(PdbFile::Dialect::AMBER_99).frames()[0];
+  EXPECT_EQ(frame.n_atoms(), 880);
+  EXPECT_EQ(frame.n_residues(), 56);
+  for (auto&& mol: frame.molecules()){
+    std::cout << mol.name().str() << " " << mol.size() << std::endl;
+  }
+}
