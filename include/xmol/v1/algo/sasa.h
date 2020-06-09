331f25aced21a71fc8ec55ea1f914db0669ded17
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-06-10 01:11:26 +0300

    Move sasa to v1

---
+++ include/xmol/v1/algo/sasa.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../fwd.h"
+#include "xmol/v1/future/span.h"
+
+namespace xmol::v1::algo {
+
+std::vector<double> calculate_sasa(const std::vector<geom::XYZ>& coords, future::Span<double> coord_radii,
+                                   double solvent_radii, int n_samples = 20,
+                                   const future::Span<int>& sasa_points_indices = {});
+
+}
--- src/xmol/v1/algo/sasa.cpp
+++ src/xmol/v1/algo/sasa.cpp
@@ -1,42 +1,30 @@
-#include "xmol/geometry/calculate_sasa.h"
+#include "xmol/v1/algo/sasa.h"
+#include <algorithm>
 #include <map>
 #include <numeric>
-#include <iostream>
-#include <xmol/geometry/basic.h>
 #include <gsl/gsl_assert>
 
-std::vector<double> xmol::geometry::calculate_sasa(
-    const std::vector<xmol::geometry::XYZ> &coords,
-    double *radii_begin,
-    double *radii_end,
-    double solvent_radii,
-    int n_samples,
-    const int *sasa_points_indices_begin,
-    const int *sasa_points_indices_end
-) {
-  if (coords.size() != std::distance(radii_begin, radii_end)) {
-    throw xmol::geometry::GeomError("xmol::geometry::calculate_sasa: coords.size() != radii.size()");
+std::vector<double> xmol::v1::algo::calculate_sasa(const std::vector<geom::XYZ>& coords,
+                                                   future::Span<double> coord_radii, double solvent_radii,
+                                                   int n_samples, const future::Span<int>& sasa_points_indices) {
+  if (coords.size() != coord_radii.size()) {
+    throw xmol::geometry::GeomError("xmol::algo::calculate_sasa: coords.size() != radii.size()");
   }
-  if (sasa_points_indices_begin != nullptr
-      && coords.size() < std::distance(sasa_points_indices_begin, sasa_points_indices_end)) {
-    throw xmol::geometry::GeomError("xmol::geometry::calculate_sasa: coords.size() < sasa_points_indices.size()");
+  if (!sasa_points_indices.empty() && coords.size() < sasa_points_indices.size()) {
+    throw xmol::geometry::GeomError("xmol::algo::calculate_sasa: coords.size() < sasa_points_indices.size()");
   }
-  const double max_radii = std::accumulate(radii_begin, radii_end, 0.0,
-                                           [](const double &a, const double &b) { return std::max(a, b); });
+  const double max_radii = std::accumulate(coord_radii.begin(), coord_radii.end(), 0.0,
+                                           [](const double& a, const double& b) { return std::max(a, b); });
   const double neighbour_cell_size = (max_radii + solvent_radii) * 2;
 
-  std::map<std::tuple<int, int, int>,
-           std::vector<int>> neighbour_index;
+  std::map<std::tuple<int, int, int>, std::vector<int>> neighbour_index;
 
-  auto cell_index = [neighbour_cell_size](const XYZ &r) -> std::tuple<int, int, int> {
-    return std::make_tuple(
-        int(std::round(r.x() / neighbour_cell_size)),
-        int(std::round(r.y() / neighbour_cell_size)),
-        int(std::round(r.z() / neighbour_cell_size))
-    );
+  auto cell_index = [neighbour_cell_size](const XYZ& r) -> std::tuple<int, int, int> {
+    return std::make_tuple(int(std::round(r.x() / neighbour_cell_size)), int(std::round(r.y() / neighbour_cell_size)),
+                           int(std::round(r.z() / neighbour_cell_size)));
   };
 
-  auto segments_length = [](std::vector<std::pair<double, double>> &segments) {
+  auto segments_length = [](std::vector<std::pair<double, double>>& segments) {
     double result = 0;
     std::sort(segments.begin(), segments.end());
     auto it = segments.begin();
@@ -62,7 +50,7 @@ std::vector<double> xmol::geometry::calculate_sasa(
     neighbour_index[cell_index(coords[i])].push_back(i);
   }
 
-  for (auto &it: neighbour_index) {
+  for (auto& it : neighbour_index) {
     int i, j, k;
     std::tie(i, j, k) = it.first;
   }
@@ -72,40 +60,32 @@ std::vector<double> xmol::geometry::calculate_sasa(
 
   const int n_neigh_cells = std::ceil((2 * (max_radii + solvent_radii) / neighbour_cell_size));
 
-  auto limit = (sasa_points_indices_begin == nullptr) ? coords.size() : std::distance(sasa_points_indices_begin,
-                                                                                    sasa_points_indices_end);
-
+  auto limit = sasa_points_indices.empty() ? coords.size() : sasa_points_indices.size();
 
   for (int i1 = 0; i1 < limit; ++i1) {
-    int n = sasa_points_indices_begin == nullptr ? i1 : sasa_points_indices_begin[i1];
+    int n = sasa_points_indices.empty() ? i1 : sasa_points_indices[i1];
     if (GSL_UNLIKELY(n < 0 && n >= coords.size())) {
-      throw xmol::geometry::GeomError(
-          "xmol::geometry::calculate_sasa: invalid index `" + std::to_string(n) + "`");
+      throw xmol::geometry::GeomError("xmol::geometry::calculate_sasa: invalid index `" + std::to_string(n) + "`");
     }
 
-    double Rn = radii_begin[n] + solvent_radii;
+    double Rn = coord_radii[n] + solvent_radii;
     double atom_area = 4 * M_PI * Rn * Rn;
 
     double delta = 2 * Rn / n_samples;
     int i, j, k;
     std::tie(i, j, k) = cell_index(coords[n]);
     std::vector<int> neigh_indecies;
-    double max_distance = (radii_begin[n] + max_radii + 2 * solvent_radii);
+    double max_distance = (coord_radii[n] + max_radii + 2 * solvent_radii);
     double max_distance2 = max_distance * max_distance;
     for (int di = -n_neigh_cells; di <= n_neigh_cells; ++di) {
       for (int dj = -n_neigh_cells; dj <= n_neigh_cells; ++dj) {
         for (int dk = -n_neigh_cells; dk <= n_neigh_cells; ++dk) {
-          auto it = neighbour_index.find(
-              std::make_tuple(
-                  i + di,
-                  j + dj,
-                  k + dk
-              ));
+          auto it = neighbour_index.find(std::make_tuple(i + di, j + dj, k + dk));
           if (it == neighbour_index.end()) {
             continue;
           }
-          for (int m: it->second) {
-            if (distance2(coords[n], coords[m]) < max_distance2) {
+          for (int m : it->second) {
+            if (coords[n].distance2(coords[m]) < max_distance2) {
               neigh_indecies.push_back(m);
             }
           }
@@ -117,11 +97,11 @@ std::vector<double> xmol::geometry::calculate_sasa(
       double Rn_ = std::sqrt(Rn * Rn - dz * dz);
       std::vector<std::pair<double, double>> segments;
 
-      for (int m: neigh_indecies) {
+      for (int m : neigh_indecies) {
         if (m == n) {
           continue;
         }
-        double Rm = radii_begin[m] + solvent_radii;
+        double Rm = coord_radii[m] + solvent_radii;
         double dz2 = coords[n].z() - coords[m].z() + dz;
         if (std::fabs(dz2) >= Rm) {
           continue;
@@ -136,8 +116,8 @@ std::vector<double> xmol::geometry::calculate_sasa(
         } // no overlap
         if (Rn_ >= d + Rm_) {
           continue;
-        } // second circle within first circle
-        if (Rm_ >= d + Rn_) {// first circle within second circle
+        }                     // second circle within first circle
+        if (Rm_ >= d + Rn_) { // first circle within second circle
           segments.emplace_back(-M_PI, M_PI);
           continue;
         }
@@ -157,7 +137,6 @@ std::vector<double> xmol::geometry::calculate_sasa(
           segments.emplace_back(l, M_PI);
           segments.emplace_back(-M_PI, r - M_PI * 2);
         }
-
       }
       atom_area -= segments_length(segments) * Rn * delta;
     }
