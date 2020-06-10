2f248d259d1745071d92d7b95a3cb69e04c79aa0
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-06-10 04:11:36 +0300

    Move pdb tests

--- tests/v1/io/pdb/PdbReader.cpp
+++ tests/v1/io/pdb/PdbReader.cpp
@@ -1,12 +1,11 @@
 #include <gtest/gtest.h>
 
-#include "xmol/pdb/PdbReader.h"
-#include "xmol/polymer/exceptions.h"
+#include "xmol/v1/io/pdb/PdbReader.h"
 
 
 using ::testing::Test;
-using namespace xmol::pdb;
-using namespace xmol::polymer;
+using namespace xmol::v1::io::pdb;
+using namespace xmol::v1;
 
 
 
@@ -19,11 +18,11 @@ TEST_F(PdbReaderTests, sound){
   PdbReader pdbReader(ss);
 
   auto frame = pdbReader.read_frame();
-  auto& chain =  frame.all()[0];
-  auto& residue =  chain.all()[0];
-  auto& atom =  residue.all()[0];
+  auto chain =  frame.molecules()[0];
+  auto residue =  frame.residues()[0];
+  auto atom =  frame.atoms()[0];
 
-  EXPECT_EQ(frame.size(),1);
+  EXPECT_EQ(frame.n_molecules(),1);
   EXPECT_EQ(chain.size(),1);
   EXPECT_EQ(chain.name(),ChainName("A"));
   EXPECT_EQ(residue.size(),1);
@@ -36,8 +35,9 @@ TEST_F(PdbReaderTests, sound){
   EXPECT_DOUBLE_EQ(atom.r().y(),86.699);
   EXPECT_DOUBLE_EQ(atom.r().z(),94.383);
 
-  EXPECT_NO_THROW(frame[ChainName("A")]);
-  EXPECT_NO_THROW(frame[ChainName("A")][ResidueId(-3,ResidueInsertionCode("Z"))]);
-  EXPECT_NO_THROW(frame[ChainName("A")][ResidueId(-3,ResidueInsertionCode("Z"))][AtomName("N")]);
-  EXPECT_THROW(frame[ChainName("B")],OutOfRangeFrame);
+  // todo: re-enable when indexing is implemented
+//  EXPECT_NO_THROW(frame[ChainName("A")]);
+//  EXPECT_NO_THROW(frame[ChainName("A")][ResidueId(-3,ResidueInsertionCode("Z"))]);
+//  EXPECT_NO_THROW(frame[ChainName("A")][ResidueId(-3,ResidueInsertionCode("Z"))][AtomName("N")]);
+//  EXPECT_THROW(frame[ChainName("B")],OutOfRangeFrame);
 }
