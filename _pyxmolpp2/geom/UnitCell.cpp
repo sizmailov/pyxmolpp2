b3feace26dde90a7bad1f358314d08ca5acec55d
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-06-24 18:52:09 +0300

    Test parsing unit cell parameters from rst7 line

--- _pyxmolpp2/geom/UnitCell.cpp
+++ _pyxmolpp2/geom/UnitCell.cpp
@@ -26,11 +26,31 @@ void pyxmolpp::v1::populate(pybind11::class_<xmol::geom::UnitCell>& pyUniCell) {
       .def("scale_by", &UnitCell::scale_by, py::arg("factor"), "Scale lattice vectors by given linear factor")
       .def("scale_to_volume", &UnitCell::scale_to_volume, py::arg("volume"))
       .def_property_readonly("volume", &UnitCell::volume)
+      .def_property_readonly("a", &UnitCell::a)
+      .def_property_readonly("b", &UnitCell::b)
+      .def_property_readonly("c", &UnitCell::c)
+      .def_property_readonly("alpha", &UnitCell::alpha)
+      .def_property_readonly("beta", &UnitCell::beta)
+      .def_property_readonly("gamma", &UnitCell::gamma)
       .def("closest_image_to", &UnitCell::closest_image_to, py::arg("ref"), py::arg("var"),
            R"pydoc(
 :param ref: reference point
 :param var: variable point
 :returns: `shift` that minimizes :math:`(ref-(var+shift))^2`
 )pydoc")
+      .def_static("from_rst7_line",
+                  [](std::string& line) {
+                    if (line.size() < 6 * 12) {
+                      throw std::runtime_error("rst7 unit cell line must be at least 72 chars long");
+                    }
+                    char* end;
+                    double a = std::strtod(line.c_str(), &end);
+                    double b = std::strtod(line.c_str() + 12, &end);
+                    double c = std::strtod(line.c_str() + 12 * 2, &end);
+                    double alpha = std::strtod(line.c_str() + 12 * 3, &end);
+                    double beta = std::strtod(line.c_str() + 12 * 4, &end);
+                    double gamma = std::strtod(line.c_str() + 12 * 5, &end);
+                    return UnitCell(a, b, c, Degrees(alpha), Degrees(beta), Degrees(gamma));
+                  })
       .def("__getitem__", &UnitCell::operator[], py::arg("i"));
 }
--- tests/bindings/geometry/test_lattice_vectors.py
+++ tests/bindings/geometry/test_lattice_vectors.py
@@ -15,6 +15,22 @@ def test_unit_cell_volume():
     assert cell[1].distance(XYZ(0, 3, 0)) == pytest.approx(0)
     assert cell[2].distance(XYZ(0, 0, 3)) == pytest.approx(0)
 
+def test_from_rst7_conversion():
+    from pyxmolpp2 import UnitCell
+    line = "  59.3881247  59.3881247  59.3881247 109.4712190 109.4712190 109.4712190"
+    cell: UnitCell = UnitCell.from_rst7_line(line)
+    print(cell[0], cell[1], cell[2])
+    print(cell[0].len(), cell[1].len(), cell[2].len())
+    print(">>", cell.a, cell.b,cell.c)
+    print(">>", cell.alpha.degrees, cell.beta.degrees, cell.gamma.degrees)
+    assert cell.a == pytest.approx(59.3881247)
+    assert cell.b == pytest.approx(59.3881247)
+    assert cell.c == pytest.approx(59.3881247)
+    assert cell.alpha.degrees == pytest.approx(109.4712190)
+    assert cell.beta.degrees == pytest.approx(109.4712190)
+    assert cell.gamma.degrees == pytest.approx(109.4712190)
+    with pytest.raises(RuntimeError):
+        cell = UnitCell.from_rst7_line("too_short")
 
 def test_closed_image():
     from pyxmolpp2 import UnitCell, XYZ
