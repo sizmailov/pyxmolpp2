1dd8cc3693e3e4fdf1c1a0890e10a619c5dd5f6a
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-06-29 04:05:20 +0300

    Add AssembleQuaternaryStructure pipe

--- pyxmolpp2/pipe/__init__.py
+++ pyxmolpp2/pipe/__init__.py
@@ -1,5 +1,6 @@
-from typing import Sequence, Callable
-from pyxmolpp2 import Frame, AtomPredicate, PdbFile, TrjtoolDatFile, Trajectory, XYZ
+from typing import Sequence, Callable, List
+from pyxmolpp2 import Frame, AtomPredicate, MoleculePredicate, PdbFile, TrjtoolDatFile, Trajectory, XYZ, CoordSelection, \
+    Molecule, AtomSelection, Translation
 
 
 class ProcessedTrajectory:
@@ -17,7 +18,7 @@ class ProcessedTrajectory:
 
 
 class Align:
-    def __init__(self, by: AtomPredicate, reference: Frame = None, move_only: AtomPredicate = None):
+    def __init__(self, by: AtomPredicate, reference: Trajectory.Frame = None, move_only: AtomPredicate = None):
         self.by = by
         self.reference = reference
         self.move_only = move_only
@@ -25,12 +26,12 @@ class Align:
         self.moved_coords = None
         self.ref_coords = None
 
-    def __ror__(self, trajectory: Sequence[Frame]):
+    def __ror__(self, trajectory: Sequence[Trajectory.Frame]):
         if self.reference is None:
             self.reference = trajectory[0]
         return ProcessedTrajectory(trajectory, self)
 
-    def __call__(self, frame: Frame):
+    def __call__(self, frame: Trajectory.Frame):
         if self.frame_coords is None:
             self.frame_coords = frame.atoms.filter(self.by).coords
             self.ref_coords = self.reference.atoms.filter(self.by).coords
@@ -47,9 +48,56 @@ class ScaleUnitCell:
         import numpy as np
         self.volume = np.genfromtxt(summary_volume_filename, usecols=[1])
 
-    def __ror__(self, trajectory: Sequence[Frame]):
+    def __ror__(self, trajectory: Sequence[Trajectory.Frame]):
+        return ProcessedTrajectory(trajectory, self)
+
+    def __call__(self, frame: Trajectory.Frame):
+        assert frame.index < len(self.volume), "Frame index is greater than supplied volume array"
+        frame.cell.scale_to_volume(self.volume[frame.index])
+
+
+class AssembleQuaternaryStructure:
+    def __init__(self, of: MoleculePredicate, by: AtomPredicate, reference: Frame = None):
+        self.molecules_selector = of
+        self.reference = reference
+        self.reference_atoms_selector = by
+
+        self.ref_first_molecule_coords: CoordSelection = None
+        self.molecules: List[Molecule] = []
+        self.molecules_coords: List[CoordSelection] = []
+        self.reference_mean_coords: List[XYZ] = []
+
+    def __ror__(self, trajectory: Sequence[Trajectory.Frame]):
+        if self.reference is None:
+            self.reference = trajectory[0]
+        mols = self.reference.molecules.filter(self.molecules_selector)
+        self.reference_mean_coords = [
+            mol.atoms.filter(self.reference_atoms_selector).coords.mean()
+            for mol in mols
+        ]
+        self.ref_first_molecule_coords = mols[0].atoms.filter(self.reference_atoms_selector).coords
+        assert len(self.reference_mean_coords) > 1, "Number of selected molecules must be greater than 1"
         return ProcessedTrajectory(trajectory, self)
 
     def __call__(self, frame: Frame):
-        assert frame.index < len(self.volume), "Frame index is greater than supplied volume array"
-        frame.cell.scale_to_volume(self.volume[frame.index])
+        if not self.molecules_coords:
+            self.molecules = [mol for mol in frame.molecules.filter(self.molecules_selector)]
+            self.molecules_coords = [
+                mol.atoms.filter(self.reference_atoms_selector).coords
+                for mol in self.molecules
+            ]
+            assert len(self.molecules_coords) == len(self.reference_mean_coords)
+            assert frame.cell.volume > 1, "Did you forget to set periodic box?"
+        # first molecule in assembly is aligned by convention
+        alignment = self.ref_first_molecule_coords.alignment_to(self.molecules_coords[0])
+        # calculate reference coordinates for current frame
+        ref_mean_coords = [alignment.transform(r) for r in self.reference_mean_coords]
+
+        # shift rest of molecules to match reference coordinates as close as possible
+        for mol_n in range(1, len(ref_mean_coords)):
+            ref_point = ref_mean_coords[mol_n]
+            mol = self.molecules[mol_n]
+            mol_point = self.molecules_coords[mol_n].mean()
+            closest = frame.cell.closest_image_to(ref_point, mol_point)
+            if closest.shift.distance(XYZ()) > 0:
+                mol.coords.apply(Translation(closest.shift))
