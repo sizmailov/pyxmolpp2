7a0e99ba43e8994d682689e3e3240dcb3054157d
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-07-04 22:47:39 +0300

    Add .data() method to future::Span and implicit construction form std::array

--- include/xmol/future/span.h
+++ include/xmol/future/span.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdlib>
 #include <vector>
+#include <array>
 
 /// Backported C++20 features
 namespace xmol::future {
@@ -11,12 +12,15 @@ template <typename T> struct Span {
   constexpr Span(T* b, T* e) : m_begin(b), m_end(e) {}
   constexpr Span(T* b, size_t n) : m_begin(b), m_end(b + n) {}
   constexpr Span(std::vector<T>& v) : Span(v.data(), v.size()) {}
+  template<size_t N>
+  constexpr Span(std::array<T, N>& v) : Span(v.data(), v.size()) {}
   T* m_begin = nullptr;
   T* m_end = nullptr;
   constexpr size_t size() const { return m_end - m_begin; };
   size_t empty() const { return m_end == m_begin; }
   T* begin() const { return m_begin; };
   T* end() const { return m_end; };
+  T* data() const { return m_begin; };
   T& operator[](size_t i) { return m_begin[i]; };
   const T& operator[](size_t i) const { return m_begin[i]; };
 
