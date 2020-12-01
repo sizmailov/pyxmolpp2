d21fa771c09abea3471720c67b85f7ee592ed296
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-12-01 11:12:01 +0300

    Detect if output file cannot be opened

--- pyxmolpp2/_core/pipe/pipe.cpp
+++ pyxmolpp2/_core/pipe/pipe.cpp
@@ -16,7 +16,8 @@ class WriteVectorsToCsv {
 public:
   using atoms_pair_t = std::pair<xmol::proxy::smart::AtomSmartRef, xmol::proxy::smart::AtomSmartRef>;
   using pair_selector_t = std::function<std::vector<atoms_pair_t>(xmol::Frame&)>;
-  using filename_provider_t = std::function<std::string(xmol::proxy::smart::AtomSmartRef&, xmol::proxy::smart::AtomSmartRef&)>;
+  using filename_provider_t =
+      std::function<std::string(xmol::proxy::smart::AtomSmartRef&, xmol::proxy::smart::AtomSmartRef&)>;
 
   WriteVectorsToCsv(pair_selector_t selector, filename_provider_t filename_formatter)
       : m_selector(std::move(selector)), m_filename_formatter(std::move(filename_formatter)) {}
@@ -27,10 +28,13 @@ public:
     const auto header = "x,y,z\n";
     m_atoms = m_selector(frame);
     m_files.reserve(m_atoms.size());
-    for (auto& atom_pair: m_atoms) {
+    for (auto& atom_pair : m_atoms) {
       std::string filename = m_filename_formatter(atom_pair.first, atom_pair.second);
-      m_files.push_back(std::make_unique<std::ofstream>(filename));
-      (*m_files.back()) << header;
+      auto& out = m_files.emplace_back(std::make_unique<std::ofstream>(filename));
+      if (!out) {
+        throw std::runtime_error("Can't open `" + filename + "` for writing");
+      }
+      (*out) << header;
     }
   }
 
@@ -62,13 +66,14 @@ private:
 void pyxmolpp::v1::populate_pipe(py::module& pipe) {
   py::class_<WriteVectorsToCsv>(pipe, "WriteVectorsToCsv")
       .def(py::init<WriteVectorsToCsv::pair_selector_t, WriteVectorsToCsv::filename_provider_t>(),
-          py::arg("pair_selector"),
-          py::arg("filename_provider"))
+           py::arg("pair_selector"), py::arg("filename_provider"))
       .def("__call__", &WriteVectorsToCsv::operator(), py::arg("frame"))
-      .def("after_last_iteration",
-           [](WriteVectorsToCsv& self, py::object&, py::object&, py::object&) -> bool {
-             return self.after_last_iteration();
-           }, py::arg("exc_type"), py::arg("exc_value"), py::arg("traceback"))
+      .def(
+          "after_last_iteration",
+          [](WriteVectorsToCsv& self, py::object&, py::object&, py::object&) -> bool {
+            return self.after_last_iteration();
+          },
+          py::arg("exc_type"), py::arg("exc_value"), py::arg("traceback"))
       .def("before_first_iteration", &WriteVectorsToCsv::before_first_iteration, py::arg("frame"))
       .def("copy", &WriteVectorsToCsv::copy);
 }
