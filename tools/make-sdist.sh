5e2a3dae8b3e6d9c40965c2f2be313324ab898de
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2023-09-05 11:29:06 +0900

    chore: Run pybind11-stubgen with `--exit-code`

--- tools/make-sdist.sh
+++ tools/make-sdist.sh
@@ -2,7 +2,7 @@
 
 pip install .
 pip install black isort pybind11-stubgen
-pybind11-stubgen pyxmolpp2._core -o ./ --numpy-array-wrap-with-annotated
+pybind11-stubgen pyxmolpp2._core -o ./ --numpy-array-wrap-with-annotated --exit-code
 black ./pyxmolpp2
 isort --profile=black ./pyxmolpp2
 python setup.py sdist
