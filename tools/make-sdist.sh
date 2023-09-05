24e59c4ef8194e3112bfa4a36e1f5a3f82e5e52d
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2023-09-05 10:57:30 +0900

    chore: Add make-sdist.sh

---
+++ tools/make-sdist.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install .
+pip install black isort pybind11-stubgen
+pybind11-stubgen pyxmolpp2._core -o ./ --numpy-array-wrap-with-annotated
+black ./pyxmolpp2
+isort --profile=black ./pyxmolpp2
+python setup.py sdist
